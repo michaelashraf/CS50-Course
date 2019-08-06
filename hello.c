94f2977ed27070a06084266fbb98a6a666d2bb41
michaelashraf <michaelashraf@users.noreply.github.com>
2019-08-06 23:12:58 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- hello.c
+++ hello.c
@@ -2,9 +2,11 @@
 #include<cs50.h>
 #include<string.h>
 
+//this program prompts the user for his/her name and then prints hello followed by that name 
+
 int main(void)
 {
-    string name=get_string("what's your name?\n");
+    string name = get_string("what's your name?\n"); //prompt for the name
     
-    printf("hello, %s\n",name);
+    printf("hello, %s\n", name); //prints hello, *name*
 }
